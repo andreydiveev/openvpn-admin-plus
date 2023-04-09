1e0b65c9f6bf2eed234001fe0ce6da99f750f0ec
andreydiveev <flat-sea@ya.ru>
2023-04-09 03:30:14 +0300

    Update pack.sh

--- build/pack.sh
+++ build/pack.sh
@@ -6,6 +6,5 @@ time docker run \
     -v "$PWD/../":/go/src/github.com/bnhf/pivpn-tap-web-ui \
     --rm \
     -w /usr/src/myapp \
-    tyzbit/beego:1.9.4 \
-    sh -c "cd /go/src/github.com/bnhf/pivpn-tap-web-ui/ && bee version && bee pack -exr='^vendor|^data.db|^build|^README.md|^docs'"
-cd github.com
+    natansdj/go-beego:1.18 \
+    sh -c "cd /go/src/github.com/bnhf/pivpn-tap-web-ui/ && go mod tidy && go mod vendor && bee version && bee pack -exr='^vendor|^data.db|^build|^README.md|^docs'"
